9836aad3804e1cbfba72b9ea0a5d5960cfe3284e
Nona <frsy@vip.qq.com>
2018-12-05 16:07:26 +0800

    add dev file

---
+++ sturct.c
@@ -0,0 +1,20 @@
+typedef struct sex
+{
+	int id;
+	char name[10];
+}Sex;
+
+typedef struct college
+{
+	int id;
+	char name[16];
+}College;
+
+typedef struct student
+{
+	int id;
+	int sid;
+	int cid;
+	char name[16];
+}Student;
+
