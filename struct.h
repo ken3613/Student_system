91a5e55de6107ad56549850e5d4bc31614acd329
ken3613 <frsy@vip.qq.com>
2018-12-06 09:33:29 +0800

    add struct.h

---
+++ struct.h
@@ -0,0 +1,26 @@
+#ifndef STU_INFO
+#define STU_INFO
+typedef struct college
+{
+	int id;
+	char name[20];
+}College;
+
+typedef struct sex
+{
+	int id;
+	char name[10];
+}Sex;
+
+typedef struct studen
+{
+	int id;
+	int sid;
+	int cid;
+	char name[16];
+	double score[10];
+	double avg;
+	int rank;
+}
+
+#endif
